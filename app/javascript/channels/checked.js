e124ece840563ca01348ecef3288bf4af9269fc0
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-07-28 11:15:11 +0900

    js check機能実装

--- app/javascript/channels/checked.js
+++
@@ -1,9 +0,0 @@
-function check() {
-  const posts = document.getElementsByClassName("post");
-  postsA = Array.from(posts);
-
-  postsA.forEach(function (post) { 
-    post.addEventListener("click", (e) => { });
-  });
-}
-window.addEventListener("load", check);
